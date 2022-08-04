ec05e1deb446d12d1133522042298b8127ec65f1
Tarun Chauhan <tarunchauhan798528@gmail.com>
2022-08-04 20:07:07 +0530

    remove comments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,3 @@ import './index.css';//universal css for all the components
 import App from './App.js';
 ReactDOM.render(<App/>,document.getElementById('root'));//compulsary right
 
-// Netflix Project app practice
-// import React from 'react';
-// import ReactDom from 'react-dom';
-// import Card from './Components/NewComponent/Card';
-
-//  ReactDom.render(<Card/>,document.getElementById('root'));
