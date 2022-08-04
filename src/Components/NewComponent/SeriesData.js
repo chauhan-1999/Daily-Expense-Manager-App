387cb9773e32c9de2531340f2344ef9ea67564e3
Tarun Chauhan <tarunchauhan798528@gmail.com>
2022-08-04 20:15:27 +0530

    Updated some code

---
+++ src/Components/NewComponent/SeriesData.js
@@ -0,0 +1,46 @@
+const SeriesData=[
+    {  
+        id:'1',
+       src:"https://i.postimg.cc/Vs2Fy4rr/stranger-things.jpg" ,
+       title:"Netflix Original Series",
+       sname:"Stranger Things" ,
+       link:"https://www.netflix.com/in/title/80057281" ,
+    },
+    {
+        id:'2',
+        src:"https://i.postimg.cc/hjsTXbpn/Dark.jpg" ,
+        title:"Netflix Original Series",
+        sname:"Dark" ,
+        link:"https://www.netflix.com/in/title/80100172" ,
+    },
+    {
+        id:'3',
+        src:"https://i.postimg.cc/GphsZsbS/Money-Heist.jpg" ,
+        title:"Netflix Original Series",
+        sname:"Money Heist" ,
+        link:"https://www.netflix.com/in/title/80192098" ,
+    },
+    {
+        id:'4',
+        src:"https://i.postimg.cc/brPMRZVy/all-of-us-are-dead.jpg" ,
+        title:"Netflix Original Series",
+        sname:"All of us are Dead" ,
+        link:"https://www.netflix.com/in/title/81237994" ,
+    },
+    {
+        id:'5',
+        src:"https://i.postimg.cc/t4srWdRd/the-witcher.jpg" ,
+        title:"Netflix Original Series",
+        sname:"The Witcher" ,
+        link:"https://www.netflix.com/in/title/80189685" ,
+    },
+    {
+        id:'6',
+        src:"https://i.postimg.cc/rsqnrvzz/peaky-blinders.jpg" ,
+        title:"Netflix Original Series",
+        sname:"Peaky Blinders" ,
+        link:"https://www.netflix.com/in/title/80002479" ,
+    },
+]
+
+export default SeriesData;
---
+++ src/Components/SubComponents.js
@@ -0,0 +1,9 @@
+import './SubComponents.css';
+function SubComponents(){
+    return (
+          <div>
+            <h3 className='col'>This is a SubComponents</h3>
+          </div>
+    );
+}
+export default SubComponents;
